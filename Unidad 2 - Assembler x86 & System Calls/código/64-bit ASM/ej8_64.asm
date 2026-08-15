d796b68af9abf4d393b33ff75e0264503e2e6232
jojohuerta <johuerta@itba.edu.ar>
2026-08-15 19:32:20 -0300

    Fix: 2-8 64

--- Unidad 2 - Assembler x86 & System Calls/código/64-bit ASM/ej8_64.asm
+++ Unidad 2 - Assembler x86 & System Calls/código/64-bit ASM/ej8_64.asm
@@ -14,8 +14,8 @@ selection_sort:
     PUSH RBP
     MOV RBP, RSP
  
-    PUSH R9     ;AUX1   
-    PUSH R10    ;AUX2
+    PUSH RBX;       ;AUX1   
+    PUSH RCX        ;AUX2
 
     CMP RSI, 1
     JLE .end
@@ -28,18 +28,21 @@ selection_sort:
     ;RDI still is the address pointing to the start of the array
     ;RSI still is the length of said array
 
-    ;Also, since there only is in the stack the CALL of selection_sort and the RBP, R9 and R10, 8 + 24 = 32
+    ;Also, since there only is in the stack the CALL of selection_sort and the RBP, RBX and RCX, 8 + 24 = 32
     ;So, the stack aligns with 16. 
 
     CALL menor
     ;Now we have in RAX the index of the lowest value.
     ;RAX = MIN_INDEX
     
-    MOV R9, [RDI]               ; AUX1 = ARRAY[0]
-    MOV R10, [RDI + 4 * RAX]    ; AUX2 = ARRAY[MIN_INDEX]
+    XOR RBX, RBX
+    XOR RCX, RCX
 
-    MOV [RDI + 4 * RAX], R9     ;ARRAY[MIN_INDEX] = AUX1
-    MOV [RDI], R10              ;ARRAY[0] = AUX2
+    MOV EBX, DWORD [RDI]                ;AUX1 = ARRAY[0]
+    MOV ECX, DWORD [RDI + 4 * RAX]      ;AUX2 = ARRAY[MIN_INDEX]
+
+    MOV [RDI + 4 * RAX], EBX            ;ARRAY[MIN_INDEX] = AUX1
+    MOV [RDI], ECX                      ;ARRAY[0] = AUX2
 
     ADD RDI, 4                  ;"ARRAY++". Remember in order to advance one space we need to increment by 4 because the array contains 4-byte numbers
     DEC RSI                     ;ARRAY_LEN--
@@ -48,8 +51,8 @@ selection_sort:
  .end: 
     
     ;Restoration of registers
-    POP R10
-    POP R9
+    POP RCX
+    POP RBX
 
     MOV RSP, RBP
     POP RBP
