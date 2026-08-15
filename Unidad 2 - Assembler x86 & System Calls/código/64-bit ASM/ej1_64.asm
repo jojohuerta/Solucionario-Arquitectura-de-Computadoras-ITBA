6095cdff53952b5ef06803cda04f5f0354206411
jojohuerta <johuerta@itba.edu.ar>
2026-08-14 23:31:49 -0300

    Feat: TP2: Ej2_g4 and updated ej1_64

--- Unidad 2 - Assembler x86 & System Calls/código/64-bit ASM/ej1_64.asm
+++ Unidad 2 - Assembler x86 & System Calls/código/64-bit ASM/ej1_64.asm
@@ -12,24 +12,24 @@ _start:
 
     ; First, we want to print the message to the screen using the write system call.
     ; To do this, we need to pass the following parameters to the system call:
-    ; rax = 1 (the system call number for sys_write in 64-bit)
-    ; rdi = 1 (the file descriptor for standard output)
-    ; rsi = memory address of the message to print
-    ; rdx = length of the message to print
+    ; RAX = 1 (the system call number for sys_write in 64-bit)
+    ; RDI = 1 (the file descriptor for standard output)
+    ; RSI = memory address of the message to print
+    ; RDX = length of the message to print
     ; Remember, we MUST USE THESE EXACT REGISTERS, because the system call 
     ; interface is defined to use these registers for these parameters.
 
-    mov rax, 1                  ; system call for write. This is how the operating system knows which system call we want to invoke.
-    mov rdi, 1                  ; file handle 1 is STDOUT. This is how the operating system knows where we want to write the message.
-    mov rsi, message            ; address of string to output. This is how the operating system knows what we want to write.
-    mov rdx, message_length     ; number of bytes. This is how the operating system knows how many bytes we want to write.
+    MOV RAX, 1                  ; system call for write. This is how the operating system knows which system call we want to invoke.
+    MOV RDI, 1                  ; file descriptor 1 is STDOUT. This is how the operating system knows where we want to write the message.
+    MOV RSI, message            ; address of string to output. This is how the operating system knows what we want to write.
+    MOV RDX, message_length     ; number of bytes. This is how the operating system knows how many bytes we want to write.
     
-    syscall                     ; invoke operating system to do the write. This is how we actually invoke the system call.
+    SYSCALL                     ; invoke operating system to do the write. This is how we actually invoke the system call.
                                 ; It is equivalent to using the instruction INT 0x80 in 32-bit mode, but in 64-bit mode we use the syscall instruction.
 
-    mov rax, 60                 ; We indicate the system call we want to use is EXIT (60 in 64-bit)
-    mov rdi, 0                  ; We set the exit code to 0
-    syscall                     ; invoke operating system to execute the exit system call.
+    MOV RAX, 60                 ; We indicate the system call we want to use is EXIT (60 in 64-bit)
+    MOV RDI, 0                  ; We set the exit code to 0
+    SYSCALL                     ; invoke operating system to execute the exit system call.
 
 
 section .data
---
+++ Unidad 2 - Assembler x86 & System Calls/código/64-bit ASM/ej2_64.asm
@@ -0,0 +1,58 @@
+section .text
+
+GLOBAL _start
+
+_start: 
+
+;First, we want to print the message to the screen using the write system call.
+
+    mov RAX, 1                  ; system call for write in 64 bit architecture.
+    mov RDI, 1                  ; file descriptor 1 is STDOUT.
+    mov RSI, string             ; address of string to output.
+    mov RDX, string_length      ; number of bytes to be printed.
+    SYSCALL                     ; invoke operating system to do the write.
+
+;We will implement the pseudo-code that was described in the comprehensive solutions manual of Unit 2.
+;First, in order to loop, we'll need to define the label in where we will jump back to. In this case, we will call it "loop_start".
+
+    mov AL, BYTE [RSI]
+.loop_start:
+    TEST AL, AL                  ; check if the current character is null terminator (0x00)
+    JZ .loop_end                 ; were it to be the case, we jump to the end of the loop
+    ;Now we will check if the current character is a lowercase letter. If it is, we will convert it to uppercase.
+    CMP AL, 0x61                 ; compare the current character with 'a' (0x61)
+    JL .not_lowercase            ; if the current character is less than 'a', it is not a lowercase letter, so we jump to the next iteration of the loop
+    CMP AL, 0x7A                 ; compare the current character with 'z' (0x7A)
+    JG .not_lowercase            ; if the current character is greater than 'z', it is not a lowercase letter, so we jump to the next iteration of the loop
+
+    ;If we reached this point, it means that the current character is a lowercase letter. We will convert it to uppercase by subtracting 32 (0x20) from its ASCII value.
+
+    SUB AL, 0x20                   ; convert lowercase letter to uppercase
+    MOV [RSI], AL                  ; store the converted character back into the string
+
+.not_lowercase:
+    ADD RSI, 1                      ; move to the next character in the string
+    MOV AL, [RSI]                   ; load the next character into AL
+    JMP .loop_start                 ; jump back to the start of the loop to process the next character
+
+.loop_end:
+;Lastly, we want to print the new string and then exit the program using the exit system call.
+
+    mov RAX, 1
+    mov RDI, 1
+    mov RSI, string
+    mov RDX, string_length
+    SYSCALL
+
+    mov RAX, 60                 ; system call for exit in 64 bit architecture.
+    mov RDI, 0                  ; exit code 0.
+    syscall                     ; invoke operating system to do the exit.
+
+section .data
+
+string db "h4ppy c0d1ng", 10, 0
+string_length equ $ - string
+
+section .bss
+
+placeholder resb 32
