1f3f6f59fa077af8af13f6c26baa8c98c60ccdb9
jojohuerta <johuerta@itba.edu.ar>
2026-08-15 22:58:49 -0300

    Feat: 2-10 32

---
+++ Unidad 2 - Assembler x86 & System Calls/código/32-bit ASM/ej10_32.asm
@@ -0,0 +1,85 @@
+section .text
+
+GLOBAL _start
+EXTERN num2str
+EXTERN strlen
+
+_start:
+    MOV EBP, ESP
+
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, message
+    MOV EDX, message_len
+    INT 80h
+
+    ;printing of number of arguments
+    MOV EBX, placeholder
+    PUSH EBX
+    PUSH DWORD [EBP]
+    CALL num2str ;Returns in EAX the length of the string
+    ADD ESP, 8 ;Cleaning of the Stack
+
+    ;printing of the number of arguments
+    MOV EDX, EAX
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, placeholder
+    INT 80h 
+
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, newline
+    MOV EDX, 1
+    INT 80h
+    
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, message2
+    MOV EDX, message2_len
+    INT 80h
+
+    ;We now start printing all the arguments
+
+.loop:
+    
+    MOV EDI, EBP     ; We're gonna use EDI to iterate
+    MOV ESI, [EDI]
+    
+    ;If it's null, we reached the end of arguments
+    TEST ESI, ESI
+    JZ loop_end
+
+    PUSH ESI
+    CALL strlen     ; Returns in EAX the length of a string
+    ADD EBP, 4
+
+    MOV EDX, EAX
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, ESI
+    INT 80h
+
+    ADD EDI, 4
+
+    JMP .loop
+
+.loop_end:
+
+    
+    ;Exit
+    MOV EAX, 1
+    MOV EBX, 0
+    INT 80h
+
+section .data
+    message DB "Number of arguments:"
+    message_len EQU $-message
+
+    newline DB 10
+
+    message2 DB "Arguments:", 10
+    message2_len EQU $-message2
+
+section .bss
+    placeholder RESB 32
---
+++ Unidad 2 - Assembler x86 & System Calls/código/32-bit ASM/ej10_strlen_32.asm
@@ -0,0 +1,37 @@
+section .text
+
+GLOBAL strlen
+
+strlen:
+    PUSH EBP
+    MOV EBP, ESP
+
+    PUSH EBX        ;We're going to use it to analyse the characters
+    PUSH ECX        ;We're going to use it to iterate
+    PUSH EDX        ;We're going to use it as a counter
+    
+    XOR EDX, EDX    ;LEN = 0
+    MOV ECX, [EBP+4];ECX = string 
+.loop:
+    MOV BL, BYTE [ECX]
+    TEST BL, BL
+    JZ .end
+
+    INC EDX
+    INC ECX
+    JMP .loop
+
+.end:
+
+    MOV EAX, EDX
+    POP EDX
+    POP ECX
+    POP EBX
+
+    MOV ESP, EBP    
+    POP EBP
+
+section .data
+
+section .bss
+    placeholder RESB 32
