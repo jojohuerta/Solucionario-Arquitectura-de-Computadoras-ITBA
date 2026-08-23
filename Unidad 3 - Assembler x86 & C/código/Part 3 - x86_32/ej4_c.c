931d1c372342ec0be2c68c684668e7e974d1a12f
jojohuerta <johuerta@itba.edu.ar>
2026-08-23 01:58:03 -0300

    Chore: 3-4 C update

--- Unidad 3 - Assembler x86 & C/código/Part 3 - x86_32/ej4_c.c
+++ Unidad 3 - Assembler x86 & C/código/Part 3 - x86_32/ej4_c.c
@@ -5,6 +5,6 @@ extern int seven(void);
 int main(void){
     printf("This should be the number seven:");
     int mysterious_number = seven();
-    printf("%d", mysterious_number);
+    printf("%d \n", mysterious_number);
     return 0;
 }
